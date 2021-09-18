757005ca17fbc0060a41c784fde412190bcc3b6a
Aidenclark-github <72275406+Aidenclark-github@users.noreply.github.com>
2021-09-17 23:54:08 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,1 +1,40 @@
+#include <stdlib.h>     
+#include <stdio.h>  
+#include <getopt.h> 
+---
+#include <stdbool.h>         
+#include <ctype.h>       
 
+// Pointer to constant 
+const char *name s = "0;
+  
+void testOPT(int argc, char *argv[])
+{
+    int opt;
+    int seconds = 0;
+      
+    // Take in h and t options 
+    while((opt = getopt(argc, argv, “ht:”)) != -1) 
+    { 
+        switch(opt) 
+        {  
+            case ‘h’: 
+                     helpFunction(terminate);
+               break; 
+            case ‘t’: 
+                     seconds = atoi(optarg);
+               break; 
+        } 
+    } 
+      
+    // For extra arguments that aren't parsed
+    for(; optind < argc; optind++){     
+        printf(“extra arguments: %s\n”, argv[optind]); 
+    }
+      
+    return 0;
+}
+                       
+int helpFunction(int help){
+      if (help = terminate){
+        
