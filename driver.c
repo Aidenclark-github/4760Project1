430a5936ecc9aaa561ef657985034cc7148f67ca
Aidenclark-github <72275406+Aidenclark-github@users.noreply.github.com>
2021-09-19 14:55:29 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -3,9 +3,10 @@
 #include <getopt.h> 
 #include <ctype.h>
 
-# Pointer to constant 
+// Pointer to constant 
 const char *logger = 0;
-  
+
+// Process the arguments 
 void testOPT(int arg_count, char *arg_vector[])
 {
     int opt;
@@ -21,21 +22,22 @@ void testOPT(int arg_count, char *arg_vector[])
                      helpFunction(EXIT_SUCCESS);
                break; 
             case ‘t’: 
-			if (seconds = atoi(*ptarg)){
+			if (seconds = atoi(*optarg)){
 				} else {
 				}                     
                break; 
         } 
     } 
       
-    # For extra arguments that aren't parsed
+   // For extra arguments that aren't parsed
     for(; optind < arg_count; optind++){     
         printf(“extra arguments: %s\n”, arg_vector[optind]); 
     }
       
     return;
 }
-                       
+
+// help function with perror
 int helpFunction(int help){
       if (help == EXIT_SUCCESS){
               printf("Invoke driver.c using the following format:\n");
@@ -45,45 +47,46 @@ int helpFunction(int help){
               printf(" [filename]: Saves messages to the specified filename\n");
               printf("\nNOTE: Default filename is messages.log.\n");
 	} else {
-              #use perror
+              // use perror
               perror("Error: Please use correct format for input: [-h] [-t sec] [logfile]");
              {
              exit(help);
 } 
-        
+ // Logger name function        
  void loggerName(const char *parsed) {
 	logger = parsed;
 }
 
-#argc will be the number of strings pointed to by argv. 
+// arg_count will be the number of strings pointed to by arg_vector 
+// Checks for arguments entered, up to 4	      
 int main(int arg_count, char **arg_vector) {
     loggerName(arg_vector[0]);
     switch(arg_count) {
         case 1 :
-                    printf("No arguments detected.\n");
+                    printf("No arguments. Please enter specified arguments.\n");
             break;
         case 2 :
-			printf("1 argument detected\n");
+			printf("1 argument has been entered: \n");
 			testOPT(arg_count, arg_vector);
             break;
         case 3 :
-			printf("2 arguments detected\n");
+			printf("2 arguments have been entered: \n");
                     	testOPT(arg_count, arg_vector);
             break;
         case 4 :
-			printf("3 arguments detected. .\n");
+			printf("3 arguments have been entered: \n");
                    	testOPT(arg_count, arg_vector);
             break;
         case 5 :
-			printf("4 arguments detected.\n");
+			printf("4 arguments have been entered: \n");
                    	testOPT(arg_count, arg_vector);
             break;
         default:
-                   	perror("Too many arguments");
+                   	perror("Error: You have entered too many arguments. ");
 			exit(EXIT_FAILURE);
     }
-	
-	printf("Main end:", arg_vector[0], arg_vector[1]);
+	printf("Output: \n");
+	printf(arg_vector[0], arg_vector[1]);
 	return 0;
 }
                
